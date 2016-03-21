84cbfec46a206c2499e86cfc65ef326dab99a526
Corina Schusheim <MsKashush@gmail.com>
2016-03-20 23:01:04 -0500

    Second Chance

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # WearableComputing
-Data collected from the accelerometers from the Samsung Galaxy S smartphone
+The input is a zip file containing data collected from the accelerometers from the Samsung Galaxy S smartphone.
 
+The script run_analysis.R will generate a new file named AverageTable.txt that contains average values for the variables that contain means and standard deviations.
+Note that the script assumes that the compressed file is in the same directory as the R script.
 
-I merge two files X_test.txt and X_train.txt. I copied the columns containing means (averages) and standard deviations.
-This was saved into a the file TidyData.csv. There are 65 columns in this data.
+The script will:
+*  Download and unzip the compressed directory getdata-projectfiles-UCI HAR Dataset.zip.
+*  Create a list of columns that contain means (averages) and standard deviations from the file features.txt.
+*  Merge the test results with the subject and activities found in X_test.txt. subject_test.txt and y_test.txt respectively.
+   - First extract the mean and standard deviation columns from X_test.txt
+   - Then merge y_test.txt with activity_labels.txt to get a descriptive label of activities.
+   - Then merge the tables together
+*  Merge the train results with the subject and activities found in X_train.txt. subject_train.txt and y_train.txt respectively.
+*  Merge the test and train tables into one table.
+*  Add columnn headers.
+*  Write the tidy dataset into a comma separated values workbook named "tidyData.csv".
+*  Calculate the average for each mean and standard deviation measurement.
+*  Write the summary into a text file names "Summary.txt"
 
-Then I took the average of each measurement and saved it in a table names AverageTable.csv.
--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,95 @@
 run_analysis<-function(){
 
+    library(utils)
     library(dplyr)
+    library(data.table)
+    
+    #Go to the github repository clone.
+    homeDir<-"C:/Users/Corina/WearableComputing"
+    setwd(homeDir)
+    
+    #Download the compressed file.
+    filePath<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(filePath, "inputData.zip", mode = "wb")
+    
+    #Unzip the compressed file.
+    unzip("inputData.zip")
     
     #Read features.txt into a table in order to get the column headings for the raw data
-    setwd("C:/Users/Corina/Dropbox/Data Science Specialization/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
-    tbl<-read.table("features.txt", sep="-", header=FALSE, fill = TRUE)
-    
-    #Deterine the number of columns.
-    colCount=nrow(tbl)
+    featuresFile<-"./UCI HAR Dataset/features.txt"
+    featuresTable<-read.table(featuresFile, sep="-", header=FALSE, fill = TRUE)
     
     #Determine which columns contain averages or standard deviations.
-    colHeadings<-grep("mean()", tbl[,2], fixed=TRUE)
-    colHeadings<-c(colHeadings, grep("std()", tbl[,2], fixed=TRUE))
-    colHeadings<-sort(colHeadings)
+    featureCol<-grep("mean()", featuresTable[,2], fixed=TRUE)
+    featureCol<-c(featureCol, grep("std()", featuresTable[,2], fixed=TRUE))
+    featureCol<-sort(featureCol)
+    
+    #Read in the activity lables.
+    activityFile<-"./UCI HAR Dataset/activity_labels.txt"
+    activityTbl<-read.table(activityFile)
     
     #Import test data.
-    testTbl<-read.delim("./test/X_test.txt", header=FALSE, sep=" ", dec = ".", fill = TRUE)
+    testFile<-"./UCI HAR Dataset/test/X_test.txt"
+    testSubject<-"./UCI HAR Dataset/test/subject_test.txt"
+    testActivity<-"./UCI HAR Dataset/test/y_test.txt"
+    
+    totalTestTbl<-read.table(testFile, header=FALSE, row.names = NULL)
+    testSubjectTbl<-read.table(testSubject, header=FALSE,row.names = NULL)
+    testActivityTbl<-read.table(testActivity, header = FALSE, row.names = NULL)
+    
+    #Get the mean and standard deviation columns from the test data
+    testTbl<-totalTestTbl[,featureCol]
+    
+    #Add activity lables to the test activity table.
+    testActivityTbl<-left_join(testActivityTbl, activityTbl, by = NULL)
+    
+    #Merge test data into one table.
+    testData<-data.table(testSubjectTbl$V1, testActivityTbl$V2)
+    testData<-data.table(testData, testTbl)
     
     #Import train data.
-    trainTbl<-read.delim("./train/X_train.txt", header=FALSE, sep=" ", dec = ".", fill = TRUE)
+    trainFile<-"./UCI HAR Dataset/train/X_train.txt"
+    trainSubject<-"./UCI HAR Dataset/train/subject_train.txt"
+    trainActivity<-"./UCI HAR Dataset/train/y_train.txt"
     
-    #Get test data's mean and standard deviation columns.
-    testData<-testTbl[,colHeadings]
+    totaltrainTbl<-read.table(trainFile, header=FALSE, row.names = NULL)
+    trainSubjectTbl<-read.table(trainSubject, header=FALSE,row.names = NULL)
+    trainActivityTbl<-read.table(trainActivity, header = FALSE, row.names = NULL)
     
-    #Get train data's mean and standard deviation columns.
-    trainData<-trainTbl[,colHeadings]
+    #Get the mean and standard deviation columns from the train data
+    trainTbl<-totaltrainTbl[, featureCol]
+    
+    #Add activity lables to the train activity table.
+    trainActivityTbl<-left_join(trainActivityTbl, activityTbl, by = NULL)
+    
+    #Merge train data into one table.
+    trainData<-data.table(trainSubjectTbl$V1, trainActivityTbl$V2)
+    trainData<-data.table(trainData, trainTbl) 
     
     #Merge the two datasets together.
-    totalData<-rbind(testData, trainData)
+    totalTable<-rbind(testData, trainData)
     
     #Add column names to the merged data 
-    names(totalData)<-sub("\\s+$", "", paste(gsub("[[:space:]]", "", gsub("[[:digit:]]", "",tbl[colHeadings,1])), gsub("()", "", tbl[colHeadings,2], fixed=TRUE), tbl[colHeadings, 3], sep=" "))
+    namesVector<-sub("\\s+$", "", paste(gsub("[[:space:]]", "", gsub("[[:digit:]]", "",featuresTable[featureCol,1])), gsub("()", "", featuresTable[featureCol,2], fixed=TRUE), featuresTable[featureCol, 3], sep=" "))
+    namesVector<-c("Subject", "Activity", namesVector)
+    names(totalTable)<-namesVector
     
-    #Summarize the data. Average each measurement.
-    AverageTable<-tbl_df(as.data.frame(cbind(names(totalData), colMeans(totalData, na.rm = TRUE))))
-    names(AverageTable)<-c("Variable", "Average")
-
-    #Write out the table of Averages.
-    write.table(AverageTable, file="AverageTable.txt", sep=",", row.names = FALSE)
+    #Sort the data.
+    totalTable<-arrange(totalTable, Subject, Activity)
+    
+    #Write out the tidy data set with all the data.
+    write.table(totalTable, file="tidyData.csv", sep=",", row.names = FALSE)
+    
+    #Melt the data
+    dataMelt<-melt(totalTable, id=c("Subject", "Activity"))
+    
+    #Group the data under test subject, activity and variable.
+    groupedData<-group_by(dataMelt, Subject, Activity, variable)
+    
+    #Calculate the mean of each variable.
+    summaryTable<-summarise(groupedData, mean(value))
+    
+    #Write out the summarized data set.
+    write.table(summaryTable, file="Summary.txt", sep=",", row.names = FALSE)
+    
 }
